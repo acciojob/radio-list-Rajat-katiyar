d7887714903d7dd8f89dabda321371207549c2a3
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-11-27 04:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+cy.get('.input').should('have.length', 3);
+describe('example to-do app', () => {  
+    it('checks css', () => {  
+        cy.visit('http://localhost:3000'); // Make sure to visit your app  
+        cy.get('label:nth-child(even) .input').should('have.css', 'color', 'rgb(255, 0, 0)'); // Check if even inputs are red  
+    });  
+});
